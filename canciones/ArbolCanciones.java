9b9b9b964056b74fdb46837a83977f2c8bd68407
Dalviux9th <enzod3339@gmail.com>
2024-11-25 12:40:29 -0300

    Encarpetado - Primeros pasos

---
+++ canciones/ArbolCanciones.java
@@ -0,0 +1,16 @@
+public class ArbolCanciones{
+    //Clase para arbol de canciones
+    //Siempre se considera que cada cancion tiene un único autor
+    private NodoCancion raiz;
+
+    // contructor arbol de canciones
+   public Arbolcanciones(){
+        this.raiz = null;
+   }
+
+   // Métodos del árbol  --------------
+   
+   public boolean insertarOrdenado(NodoCancion nuevoNodo) {
+     // Inserta un nuevo nodo (recibe como parámetro) ordenado alfabéticamente por título.
+   }
+}
